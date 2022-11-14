67d466e582ea4a338c4bb63098f7a78fb82fef6d
Rubén <rubenjr0@protonmail.com>
2022-11-14 03:33:06 +0100

    ⚗️ Stream splitting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::{collections::HashSet, io::Write, time::Instant};
+use std::{
+    collections::HashSet,
+    io::SeekFrom,
+    sync::{Arc, RwLock},
+    time::Instant,
+};
 
 use tokio::{
     fs::File,
-    io::{AsyncBufReadExt, BufReader},
+    io::{AsyncBufReadExt, AsyncSeekExt, BufReader},
 };
 
 use tokio_stream::{wrappers::LinesStream, Stream};
 
 use clap::{Parser, ValueEnum};
 use eyre::Result;
-use futures::StreamExt;
+use futures::{future::try_join_all, StreamExt};
 use sha2::{Digest, Sha256, Sha512};
 
 type Hash = Vec<u8>;
@@ -29,6 +34,13 @@ struct Args {
     wordlist_path: String,
 }
 
+async fn read_file_stream(path: &str) -> Result<impl Stream<Item = String>> {
+    let f = File::open(path).await?;
+    let reader = BufReader::new(f);
+    let line_stream = LinesStream::new(reader.lines()).map(|l| l.unwrap());
+    Ok(line_stream)
+}
+
 async fn read_hashes(path: &str) -> Result<Vec<Hash>> {
     let hashes = read_file_stream(path).await?;
     let hashes: HashSet<Hash> = hashes
@@ -36,6 +48,22 @@ async fn read_hashes(path: &str) -> Result<Vec<Hash>> {
         .collect()
         .await;
     Ok(hashes.into_iter().collect())
+}
+
+async fn read_wordlist(path: &str) -> Result<Vec<impl Stream<Item = String>>> {
+    let f = File::open(path).await?;
+    let s = f.metadata().await?.len() as usize;
+    let n = num_cpus::get();
+    println!("{n} CPUs");
+    let spn = s / n;
+    let mut streams = Vec::with_capacity(n);
+    for i in 0..n {
+        let f = File::open(path).await?;
+        let mut r = BufReader::with_capacity(spn, f);
+        r.seek(SeekFrom::Start((i * spn) as u64)).await?;
+        streams.push(LinesStream::new(r.lines()).map(|l| l.unwrap()));
+    }
+    Ok(streams)
 }
 
 #[inline]
@@ -55,56 +83,55 @@ fn gen_hash(data: &[u8], hash_mode: HashMode) -> Hash {
     }
 }
 
-async fn read_file_stream(path: &str) -> Result<impl Stream<Item = String>> {
-    let f = File::open(path).await?;
-    let reader = BufReader::new(f);
-    let line_stream = LinesStream::new(reader.lines()).map(|l| l.unwrap());
-    Ok(line_stream)
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
-    let mut hashes = read_hashes(&args.hashes_path).await?;
+    let hashes = read_hashes(&args.hashes_path).await?;
     println!("{} hashes loaded", hashes.len());
+    let hashes = RwLock::new(hashes);
+    let hashes = Arc::new(hashes);
 
     let read_time = Instant::now();
-    let mut wordlist = read_file_stream(&args.wordlist_path).await?;
+    let wordlist = read_wordlist(&args.wordlist_path).await?;
     let read_time = read_time.elapsed();
     println!(
-        "Wordlist stream {} read in {read_time:?}",
+        "Wordlist streams for {} read in {read_time:?}",
         args.wordlist_path
     );
 
-    let mut stdout_lock = std::io::stdout().lock();
     let crack_time = Instant::now();
-    loop {
-        if hashes.len() == 0 {
-            break;
-        }
-        if let Some(password) = wordlist.next().await {
-            for (hash_idx, hash) in hashes.iter().enumerate() {
-                if &gen_hash(password.as_bytes(), args.hash_mode) == hash {
-                    writeln!(
-                        stdout_lock,
-                        "{} --- {password:<16} [{:>14?}]",
-                        hex::encode(hash),
-                        crack_time.elapsed()
-                    )?;
-                    stdout_lock.flush()?;
-                    hashes.remove(hash_idx);
+    let mut tasks = Vec::new();
+    for mut chunk in wordlist {
+        let hashes = hashes.clone();
+        let task = tokio::spawn(async move {
+            loop {
+                if hashes.read().unwrap().len() == 0 {
                     break;
                 }
+                if let Some(password) = chunk.next().await {
+                    let h = hashes.read().unwrap().clone();
+                    for (hash_idx, hash) in h.iter().enumerate() {
+                        if &gen_hash(password.as_bytes(), args.hash_mode) == hash {
+                            println!(
+                                "{} --- {password:<16} [{:>14?}]",
+                                hex::encode(hash),
+                                crack_time.elapsed()
+                            );
+                            hashes.write().unwrap().remove(hash_idx);
+                            break;
+                        }
+                    }
+                }
             }
-        } else {
-            break;
-        }
+        });
+        tasks.push(task);
     }
+    try_join_all(tasks).await?;
     let crack_time = crack_time.elapsed();
-
+    let hashes = hashes.read().unwrap();
     if hashes.len() > 0 {
         println!("\nNo password found for the given hashes (search took {crack_time:6?}):");
-        for hash in hashes {
+        for hash in hashes.iter() {
             println!("> {}", hex::encode(hash));
         }
     }
